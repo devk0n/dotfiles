967d6da8cce39acad47021028a8c6f31c94d16ba
devk0n <27301010+devk0n@users.noreply.github.com>
2025-09-19 23:09:09 +0200

    added dap

--- nvim/lua/devkit/lsp.lua
+++ nvim/lua/devkit/lsp.lua
@@ -1,3 +1,41 @@
+-- Diagnostic config (icons, virtual text, floating, etc.)
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN]  = " ",
+      [vim.diagnostic.severity.HINT]  = "󰠠 ",
+      [vim.diagnostic.severity.INFO]  = " ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO]  = "DiagnosticSignInfo",
+    },
+  },
+  virtual_text = {
+    prefix = "●", -- or "" if you only want signs
+  },
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+  },
+})
+
+-- Diagnostic config (floating windows, virtual text, etc.)
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- set "" if you want only signs
+  },
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+  },
+})
+
 -- Capabilities (completion, snippets, etc.)
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
---
+++ nvim/lua/devkit/plugins/dap.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- Adapter: native GDB (requires GDB >= 9.2)
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" },
+      }
+
+      -- Configurations: C / C++ / Rust
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtEntry = false,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+
+      -- Keymaps (basic debugging workflow)
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP Continue" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "DAP Conditional Breakpoint" })
+      vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "DAP REPL" })
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+
+      -- Auto open/close UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
